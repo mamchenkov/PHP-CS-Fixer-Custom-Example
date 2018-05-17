e86f40e06fba31b8dc1cbe5ea31e890188353626
Leonid Mamchenkov <leonid@mamchenkov.net>
2018-05-17 10:45:11 +0300

    Added test data
    * Original.php - initial source file
    * Expected.php - expected source file
    
    The only change in Expected.php from the specification is the use
    of `null` as default value for function parameters, since we don't
    know the type of the parameter and therefor cannot properly change
    defaults to integers (0) or strings ('').

---
+++ tests/data/Original.php
@@ -0,0 +1,12 @@
+<?php 
+function test_a($id,$txt){
+        $id=$id*1;
+        if($id==0)$id=10;
+        if($txt=='')$txt='Added';
+        $arr=[];
+        $arr[out]="$id One";
+        $arr[out].="Two $txt";
+        $res=$arr[out];
+        return $res;
+}
+echo test_a();

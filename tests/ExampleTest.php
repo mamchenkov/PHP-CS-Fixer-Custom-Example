ee25364556f34a626a596d4f79d333c78a2c4df2
Leonid Mamchenkov <leonid@mamchenkov.net>
2018-05-17 10:42:02 +0300

    Added example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+	/**
+	 * Example unit test
+	 */
+	public function testTest(): void
+	{
+		$this->assertTrue(true, "The truth is gone");
+	}
+}
